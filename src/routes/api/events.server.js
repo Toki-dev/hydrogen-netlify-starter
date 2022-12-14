1556e5a9ab0079ecbfda0e7c2086931b5a179088
sandor <sandor@holomeeting.io>
2022-12-14 14:02:39 +0100

    test1

---
+++ src/routes/api/events.server.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+
+ const eventSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  startDate: Date,
+  location: String,
+  links: String,
+  city: String,
+  approved: Boolean,
+  userId: String,
+});
+
+
+ const EventsModel = mongoose.model('Event', eventSchema);
+/* 
+
+ */
+export async function api() {
+
+  mongoose.connect('mongodb+srv://sandor:xZj4EJFn9cPrrI0H@cluster0.gcspyje.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+); 
+  return await EventsModel.find({});
+}
