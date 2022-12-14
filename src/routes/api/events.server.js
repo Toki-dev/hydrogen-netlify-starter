814ade7b98b3a9d2425f41297032d2442e534d44
sandor <sandor@holomeeting.io>
2022-12-14 19:13:47 +0100

    ssss

--- src/routes/api/events.server.js
+++ src/routes/api/events.server.js
@@ -12,24 +12,39 @@ import mongoose from 'mongoose';
 });
 
 
- const EventsModel = mongoose.model('Event', eventSchema);
 
-/*  */
-export async function api(request, {params, queryShop}) {
-try {
-  mongoose.connect(process.env.MONGODB_URI , {
+
+
+ let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await   mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://sandor:xZj4EJFn9cPrrI0H@cluster0.gcspyje.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
   );
+ 
+  cachedDb = client.model('Event', eventSchema);
 
+  return cachedDb;
+};
+
+/*  */
+export async function api(request, {params, queryShop}) {
+try {
+
+  await connectToDatabase()
   if (request.method == 'POST') {
     const x = JSON.parse(request.body)
-    return await EventsModel.create({name:x.name,description: x.description});
+    return await cachedDb.create({name:x.name,description: x.description});
   }
 
 
-  return await EventsModel.find();
+  return await cachedDb.find();
 } catch (error) {
   return error
 }
