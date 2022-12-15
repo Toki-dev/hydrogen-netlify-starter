2c64b34ea408875cf94acb9b2a6866dcb221f7a7
sandor <sandor@holomeeting.io>
2022-12-15 16:20:39 +0100

    ss

--- src/routes/events/index.server.tsx
+++ src/routes/events/index.server.tsx
@@ -21,15 +21,23 @@ export default function Blog({response}: HydrogenRouteProps) {
 
   
  
+/*   const data = useQuery(
+    ['unique', 'key'], // A string or an array to uniquely identify the query.
+    async () => {
+      await main();
+      /* return await mongoose.model('Events', eventSchema).find();
+    }
+  );  */
 
 
+console.log('response', response.url)
   const xx = useQuery(
     ['unique', 'key'], // A string or an array to uniquely identify the query.
     async () => {
   try {
     const res = await fetch(`${new URL(response.url).origin}/api/events`)
     const data = await res.json()
-  
+     console.log('ressssss-------------', data) 
 return data
 
     
@@ -46,7 +54,36 @@ return data
 
  console.log('eventdata', xx.data) 
 
+  if(!xx?.data) {
+    return (
+      <Layout>
+      <Seo type="page" data={{title: 'All Journals'}} />
+      <PageHeader heading={BLOG_HANDLE} className="gap-0">
+        <Link to="/events/add">
+          <Text color="subtle">Create event</Text>
+        </Link>
+xx {typeof xx.error}
+ff {xx.error}
+{Object.keys(xx).map(x =>(<div>{x}</div>))}
+      </PageHeader>
+    </Layout>
+    )
+  }
 
+  if(!xx.data?.documents?.length) {
+    return (
+      <Layout>
+      <Seo type="page" data={{title: 'All Journals'}} />
+      <PageHeader heading={BLOG_HANDLE} className="gap-0">
+        <Link to="/events/add">
+          <Text color="subtle">Create event</Text>
+        </Link>
+hh {typeof xx.data}
+{Object.keys(xx.data?.documents).map(x =>(<div>{x}</div>))}
+      </PageHeader>
+    </Layout>
+    )
+  }
 
   return (
     <Layout>
